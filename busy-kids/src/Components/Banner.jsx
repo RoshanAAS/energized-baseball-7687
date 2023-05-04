acf6c730f679bdff83f8e55ad818ca9e4ccea21f
Roshan Kr Routh <roshan555routh@gmail.com>
2023-05-04 22:40:03 +0530

    day-2 banner update

--- busy-kids/src/Components/Banner.jsx
+++ busy-kids/src/Components/Banner.jsx
@@ -1,30 +1,48 @@
-import { Box, Container, HStack, Image } from '@chakra-ui/react'
-import React from 'react'
-import BannerImg from '../Logo/BannerImg.jpg'
-import { AspectRatio } from '@chakra-ui/react'
+import { Box, Container, HStack, Heading, Image,Text } from "@chakra-ui/react";
+import React from "react";
+import BannerImg from "../Logo/BannerImg.jpg";
+import { AspectRatio } from "@chakra-ui/react";
+import logo from "../Logo/BusyKids.png";
+import styled from "@emotion/styled";
 const Banner = () => {
-    return (
-        <Box w={"80%"} style={{
-            border: '1px solid red',
-            margin: 'auto',
-            height: '300px'
-        }}>
-            <HStack>
-                <Box>
-                    <Container>
-                        Bankaroo is a virtual bank for kids, where you can teach them about the value of money in a safe environment.
-                        <br />
-                        Kids will learn how to budget, save up for goals and spend responsibly using our cool easy-to-use app and website.
-                    </Container>
-                </Box>
-                <Box>
-                    {/* <Image src={BannerImg} alt='banner' width={'80%'} /> */}
+  return (
+    <DIV>
+    <Box
+      w={"95%"}
+      style={{
+        // border: "1px solid red",
+        margin: "auto",
+        // height: "500px",
+      }}
+    >
+      <HStack spacing={10}>
+        <Box textAlign={'center'}>
+        
+            <Image w={280} src={logo} />
+            <Heading as="h2" size="lg">
+              BUSY KID FOR SCHOOL EDITION
+            </Heading>
+            <br/>
+            <Text fontSize={"lg"} as={'sample'} lineHeight={7}>
+            Designed and developed with the help of teachers and school
+            administrators to easily manage student accounts and to teach math
+            and financial education. Built on top of the Bankaroo platform, kids
+            enjoy the unique, kid-friendly experience loved by hundreds of
+            thousands of members, while teachers have control over the students’
+            accounts and activities. Create your free account (one free account
+            per school).
+            </Text>
+           
+        
+        </Box>
+        <Box>
+          {/* <Image src={BannerImg} alt='banner' width={'80%'} /> */}
 
-                    {/* Abhi */}
-                    {/* <AspectRatio maxW='100%' ratio={16/9}>
+          {/* Abhi */}
+          {/* <AspectRatio maxW='100%' ratio={16/9}>
                     <iframe width="50%" height="60%" src="https://www.youtube.com/embed/d-mKp1qZjek" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </AspectRatio> */}
-                    {/* <Box
+          {/* <Box
                         as='iframe'
                         src='https://www.youtube.com/embed/d-mKp1qZjek'
                         width='100%'
@@ -32,16 +50,28 @@ const Banner = () => {
                             aspectRatio= '20/20'
                         // }}
                     /> */}
-                    <video width="100%" height="90%" controls>
-                        <source src="https://youtu.be/d-mKp1qZjek"  />
-                            {/* <source src="movie.ogg" type="video/ogg" /> */}
-                                {/* Your browser does not support the video tag. */}
-                            </video>
+          <iframe
+            width="700"
+            height="400"
+            src={`https://www.youtube.com/embed/d-mKp1qZjek`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        </Box>
+      </HStack>
+    </Box>
+    </DIV>
+  );
+};
 
-                        </Box>
-                    </HStack>
-                </Box>
-                )
-}
+export default Banner;
 
-                export default Banner
+const DIV=styled.div`
+  
+  width: 100%;
+ 
+  /* background-color: #e6e8d2; */
+  padding: 100px;
+
+`
