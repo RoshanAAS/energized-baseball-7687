1c4222ed955eebf512e6b4a08e435dc59d131144
ASUS <abhiraskar@gmail.com>
2023-05-05 20:07:10 +0530

    Landing Page1

--- busy-kids/src/Components/Navbar.jsx
+++ busy-kids/src/Components/Navbar.jsx
@@ -306,13 +306,13 @@ const NAV_ITEMS = [
     children: [
       {
         label: "Explore Design Work",
-        subLabel: "Trending Design to inspire you",
-        href: "#",
+        // subLabel: "Trending Design to inspire you",
+        // href: "#",
       },
       {
         label: "New & Noteworthy",
-        subLabel: "Up-and-coming Designers",
-        href: "#",
+        // subLabel: "Up-and-coming Designers",
+        // href: "#",
       },
     ],
   },
@@ -321,13 +321,9 @@ const NAV_ITEMS = [
     children: [
       {
         label: "Job Board",
-        subLabel: "Find your dream design job",
-        href: "#",
       },
       {
         label: "Freelance Projects",
-        subLabel: "An exclusive list for contract work",
-        href: "#",
       },
     ],
   },
