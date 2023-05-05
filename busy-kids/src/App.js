aab35b1820e05d5e80804c6a53c3433507cdf43f
Roshan Kr Routh <roshan555routh@gmail.com>
2023-05-05 23:02:31 +0530

    heir page

--- busy-kids/src/App.js
+++ busy-kids/src/App.js
@@ -1,20 +1,24 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./Components/Footer";
-import MainRoutes from "./Components/MainRoutes";
+// import MainRoutes from "./Components/MainRoutes";
 import Navbar from "./Components/Navbar";
-import SigninCard from "./Components/authcom/signin/SigninCard";
+// import SigninCard from "./Components/authcom/signin/SigninCard";
 // import { Calculator } from "./Pages/calculator";
-import SignupCard from "./Components/authcom/signup/Signup";
+// import SignupCard from "./Components/authcom/signup/Signup";
+import { Hire } from "./Pages/Hire";
+
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Calculator/> */}
+      {/* <Calculator/>
       <SigninCard />
       <SignupCard />
 
-      <MainRoutes />
+      <MainRoutes /> */}
+       <Hire/>
       <Footer />
     </div>
   );
