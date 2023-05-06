4e4eb28d672e29fdde6c8f4792cd0d0c52a76d89
Roshan Kr Routh <roshan555routh@gmail.com>
2023-05-07 01:12:58 +0530

    update

--- busy-kids/src/Components/TeacherList.jsx
+++ busy-kids/src/Components/TeacherList.jsx
@@ -4,15 +4,20 @@ import TeacherCard from "./TeacherCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getTeacherData } from "../redux/hireReducer/action";
 import { Progress,Box } from '@chakra-ui/react'
+import { useSearchParams } from "react-router-dom";
 const TeacherList = () => {
+  const [searchParams]=useSearchParams()
   const data = useSelector((store) => store.teacherReducer.teacherData);
   const isLoading = useSelector((store) => store.teacherReducer.isLoading);
+ 
 
   const dispatch = useDispatch();
 
   const paramObj = {
     params: {
+       q:searchParams.get('search'),
       _limit: 12,
+
     },
   };
 
