386de1c7f01cf17ea0b00096afded8f009b5725b
tejareddy9133 <reddyvaritejeshkumarreddy@gmail.com>
2023-05-07 00:40:16 +0530

    Total signup works

--- busy-kids/src/App.js
+++ busy-kids/src/App.js
@@ -3,12 +3,11 @@ import Footer from "./Components/Footer";
 import MainRoutes from "./Components/MainRoutes";
 import Navbar from "./Components/Navbar";
 
-
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MainRoutes/>
+      <MainRoutes />
       <Footer />
     </div>
   );
