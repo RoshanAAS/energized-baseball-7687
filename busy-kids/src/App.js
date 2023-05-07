22e618aefe2bf19d1e197db06ebb5a4c083c90bb
Mnv17 <maanavverma0@gmail.com>
2023-05-07 21:47:10 +0530

    day-5 work

--- busy-kids/src/App.js
+++ busy-kids/src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
-
-import SignupCard from "./Components/authcom/signup/Signup";
-// import Pricing from "./pages/Pricing";
 import Footer from "./Components/Footer";
 import MainRoutes from "./Components/MainRoutes";
-
 import Navbar from "./Components/Navbar";
-import { Hire } from "./pages/Hire";
 
-// import { Calculator } from "./Pages/calculator";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      
+      <Navbar />     
       <MainRoutes/>
       <Footer />
     </div>
