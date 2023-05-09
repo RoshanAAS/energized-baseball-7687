32ec2fa1f35319ec992234e2e51c3931890a8b9c
Roshan Kr Routh <roshan555routh@gmail.com>
2023-05-09 11:41:46 +0530

    login redirect issue resolved

--- busy-kids/src/Components/PrivateRoute.jsx
+++ busy-kids/src/Components/PrivateRoute.jsx
@@ -1,14 +1,14 @@
 
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
 
   const isAuth = useSelector((state) => state.authReducer.isAuth);
-  const location= useLocation();
 
-    return !isAuth? <Navigate state={location.pathname} to="/login" replace />:children
+
+    return !isAuth? <Navigate  to="/login"  />:children
   
 
 }
